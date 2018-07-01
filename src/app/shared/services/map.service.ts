a95d5ad74c7b3f8474e980e2d8c633eaaca76ae3
BarinDwalin <sergeishipwright@mail.ru>
2018-07-01 23:07:11 +0300

    Из сервиса карты вынесена генерация врагов

--- src/app/shared/services/map.service.ts
+++ src/app/shared/services/map.service.ts
@@ -9,6 +9,7 @@ import { GameService } from './game.service';
 import { PlayerService } from './player.service';
 import { Random } from './random';
 import { SettingsService, GameMode } from './settings.service';
+import { EnemyGroupFabric } from '@app/shared/fabrics';
 
 @Injectable()
 export class MapService {
@@ -102,24 +103,25 @@ export class MapService {
     if (this.cellIsEmpty(x, y - 1)) this.createEmptyCell(x, y - 1);
 
     let check = function (xDiff: number, yDiff: number) {
-      if (that.gameMap[x + xDiff][y + yDiff].deep == null) {
-        that.gameMap[x + xDiff][y + yDiff].isWall = waysCount <= 0;
-        that.gameMap[x + xDiff][y + yDiff].deep = deep + 1;
+      const cell = that.gameMap[x + xDiff][y + yDiff];
+      if (cell.deep == null) {
+        cell.isWall = waysCount <= 0;
+        cell.deep = deep + 1;
         waysCount -= 1;
-        if (!that.gameMap[x + xDiff][y + yDiff].isWall) {
+        if (!cell.isWall) {
           if (that.settingsService.gameMode === GameMode.Easy) {
-            that.createMostersCasual(x + xDiff, y + yDiff);
+            EnemyGroupFabric.createMostersCasual(cell);
           }
           else {
-            that.createMostersTrueHard(x + xDiff, y + yDiff);
+            EnemyGroupFabric.createMostersTrueHard(cell);
           }
         }
       }
     }
-    var checkRight = function () { check(1, 0); }
-    var checkLeft = function () { check(-1, 0); }
-    var checkTop = function () { check(0, 1); }
-    var checkBottom = function () { check(0, -1); }
+    const checkRight = () => { check(1, 0); }
+    const checkLeft = () => { check(-1, 0); }
+    const checkTop = () => { check(0, 1); }
+    const checkBottom = () => { check(0, -1); }
     // генерация путей в произвольном порядке
     let directionsWithRandomSort = [checkRight, checkLeft, checkTop, checkBottom]
       .map(p => { return { weight: Random.getFloat(0, 1), func: p } })
@@ -127,63 +129,5 @@ export class MapService {
     for (let i = 0; i < 4; i++) {
       directionsWithRandomSort[i].func();
     }
-  }
-  private createMostersCasual(x: number, y: number) {
-    let deep = this.gameMap[x][y].deep;
-    console.log('createMostersCasual', this.gameMap[x][y]);
-    //Число монстров 1 и 2лвл от глубины (min / max):
-    let s = MonstersSettings.find(p => p.deep === deep);
-    if (!s) {
-      s = {
-        deep: deep,
-        mosterLevel1Min: deep / 3,
-        mosterLevel1Max: deep / 3 + 1,
-        mosterLevel2Min: deep / 3,
-        mosterLevel2Max: deep / 3 + 1,
-        percentBoss: 33
-      };
-    }
-
-    let mosterLevel2Count = Random.getInt(s.mosterLevel2Min, s.mosterLevel2Max);
-    let diff = mosterLevel2Count - s.mosterLevel2Min;
-    let mosterLevel1Count = Random.getInt(s.mosterLevel1Min - diff * 2, s.mosterLevel1Max - diff * 2);
-    let existsBoss = mosterLevel2Count > 0 && s.percentBoss > Random.getInt(1, 100);
-    mosterLevel2Count -= existsBoss ? 1 : 0;
-
-    this.gameMap[x][y].mosterLevel1Count = mosterLevel1Count;
-    this.gameMap[x][y].mosterLevel2Count = mosterLevel2Count;
-    this.gameMap[x][y].existsBoss = existsBoss;
-  }
-  private createMostersTrueHard(x: number, y: number) {
-    let s = {
-      mosterMinCount: 1,
-      mosterMaxCount: this.gameMap[x][y].deep,
-      mosterLevel2Min: 0,
-      mosterLevel2Max: this.gameMap[x][y].deep,
-      percentBoss: 10
-    };
-    if (s.mosterMaxCount <= 3) {
-      s.mosterMaxCount += 1;
-    }
-    else if (s.mosterMaxCount < 7) {
-      s.mosterMinCount = 3;
-      s.mosterLevel2Min = 1;
-      s.percentBoss = 18;
-    }
-    else {
-      s.mosterMinCount = 3;
-      s.mosterMaxCount -= 2;
-      s.mosterLevel2Min = 3;
-      s.percentBoss = 33;
-    }
-    let mosterCount = Random.getInt(s.mosterMinCount, s.mosterMaxCount);
-    let mosterLevel2Count = Random.getInt(s.mosterLevel2Min, s.mosterLevel2Max);
-    let mosterLevel1Count = mosterCount - mosterLevel2Count < 0 ? 0 : mosterCount - mosterLevel2Count;
-    let existsBoss = mosterLevel2Count > 0 && s.percentBoss > Random.getInt(1, 100);
-    mosterLevel2Count -= existsBoss ? 1 : 0;
-
-    this.gameMap[x][y].mosterLevel1Count = mosterLevel1Count;
-    this.gameMap[x][y].mosterLevel2Count = mosterLevel2Count;
-    this.gameMap[x][y].existsBoss = existsBoss;
   }
 }
