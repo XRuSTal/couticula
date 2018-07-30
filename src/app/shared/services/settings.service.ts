704ac0d05b8dda35a4fab6579c28518e7703c5f2
BarinDwalin <BarinDwalin@live.ru>
2018-07-30 19:57:51 +0300

    Добавлен перерасчет размеров поля исходя из размеров экрана

--- src/app/shared/services/settings.service.ts
+++ src/app/shared/services/settings.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { GameMode } from '@shared/enums';
 
 @Injectable()
 export class SettingsService {
   apiUrl = '/api';
   gameMode: GameMode = GameMode.Hard;
-  countCellVisibleX: number = 11;   //TODO настроить адаптивный дизайн
-  countCellVisibleY: number = 6;   //TODO настроить адаптивный дизайн
-  sizeCell: number = 48; //размеры картинок
-  sizeIcon: number = 48; //размеры иконок
-  sizeMap: number = /*countCellVisible*/ 4 * 2 + 1;
-  sizeField: number = 48 * (4 * 2 + 1); /*sizeCell * sizeMap*/
-
+  countCellVisibleX = 11;   // TODO настроить адаптивный дизайн
+  countCellVisibleY = 6;   // TODO настроить адаптивный дизайн
   startGold: number = this.gameMode === GameMode.Easy ? 1500 : 4000;
-  priceSecondHero: number = 800;
-  priceThirdHero: number = 1000;
+  priceSecondHero = 800;
+  priceThirdHero = 1000;
+
+  private platformWidth: number;
+  private platformHeight: number;
+  private headerCoefficient = 0.1; // доля карты, выделенная под меню
+
+  constructor(platform: Platform) {
+    platform.ready().then((readySource) => {
+      this.platformWidth = platform.width();
+      this.platformHeight = platform.height();
+      this.calcFieldSize();
+    });
+  }
+
+  private calcFieldSize() {
+    const fieldSizeWithoutMenu = this.platformHeight * (1 - this.headerCoefficient);
+    const proportion = this.platformWidth / fieldSizeWithoutMenu;
+    this.countCellVisibleX = Math.floor(this.countCellVisibleY * proportion);
+  }
 }
