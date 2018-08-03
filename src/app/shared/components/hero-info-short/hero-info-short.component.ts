1f25c6b8378c953f8450d24b69e2bd5bb0b21405
BarinDwalin <BarinDwalin@live.ru>
2018-08-03 12:27:36 +0300

    Перенос компонента в разделяемые

---
+++ src/app/shared/components/hero-info-short/hero-info-short.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+import { Hero } from '@models';
+
+@Component({
+  selector: 'hero-info-short',
+  templateUrl: 'hero-info-short.component.html',
+})
+export class HeroInfoShortComponent {
+  @Input() hero: Hero;
+  @Input() isSelected: boolean;
+}
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,10 +1,13 @@
 import { GoldComponent } from './gold/gold.component';
+import { HeroInfoShortComponent } from './hero-info-short/hero-info-short.component';
 
 const SHARED_COMPONENTS: any[] = [
-  GoldComponent
-]
+  GoldComponent,
+  HeroInfoShortComponent,
+];
 
 export {
   SHARED_COMPONENTS,
-  GoldComponent
-}
+  GoldComponent,
+  HeroInfoShortComponent,
+};
