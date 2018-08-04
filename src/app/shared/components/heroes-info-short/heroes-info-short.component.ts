c08836118302b258636176da9a6133bbd9f8dd10
BarinDwalin <BarinDwalin@live.ru>
2018-08-04 23:32:41 +0300

    Компонент переключения персонажей

---
+++ src/app/shared/components/heroes-info-short/heroes-info-short.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { Hero } from '@models';
+
+const maxHeroCount = 3;
+
+@Component({
+  selector: 'heroes-info-short',
+  templateUrl: 'heroes-info-short.component.html',
+})
+export class HeroesInfoShortComponent implements OnInit {
+  @Input() heroes: Hero[];
+  @Input() isNewHeroAvailable = true;
+  @Output() addedHero = new EventEmitter<void>();
+  @Output() selectedHero = new EventEmitter<Hero>();
+
+  private currentHero: Hero;
+  get newHeroButtons() {
+    const buttons = [];
+    buttons.length = maxHeroCount - this.heroes.length;
+    buttons.fill(null);
+    return buttons;
+  }
+/*
+  constructor() {
+    this.currentHero = this.heroes[0];
+  } */
+  ngOnInit() {
+    this.currentHero = this.heroes[0];
+  }
+  addHero() {
+    this.addedHero.emit();
+  }
+  selectHero(hero: Hero) {
+    this.currentHero = hero;
+    this.selectedHero.emit(hero);
+  }
+}
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,13 +1,16 @@
 import { GoldComponent } from './gold/gold.component';
 import { HeroInfoShortComponent } from './hero-info-short/hero-info-short.component';
+import { HeroesInfoShortComponent } from './heroes-info-short/heroes-info-short.component';
 
 const SHARED_COMPONENTS: any[] = [
   GoldComponent,
   HeroInfoShortComponent,
+  HeroesInfoShortComponent,
 ];
 
 export {
   SHARED_COMPONENTS,
   GoldComponent,
   HeroInfoShortComponent,
+  HeroesInfoShortComponent,
 };
