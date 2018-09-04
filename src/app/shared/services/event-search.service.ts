2388e37f11c04d5d159ed64a1d3979e8d1bbb7d3
BarinDwalin <sergeishipwright@mail.ru>
2018-09-04 23:57:52 +0300

    Схема генерации событий на карте

--- src/app/shared/services/event-search.service.ts
+++ src/app/shared/services/event-search.service.ts
@@ -2,7 +2,19 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 
-import { Cell } from '@models';
+import { Cell, Hero } from '@models';
+import { HeroService } from './hero.service';
+import { Random } from './random';
+
+enum TypeEvent {
+  Nothing,
+  Smth,
+  AllHeroes,
+  SeparateHeroes,
+  CheckHero,
+  SelectedHero,
+  NotSelectedHero,
+}
 
 @Injectable()
 export class EventSearchService {
@@ -10,12 +22,56 @@ export class EventSearchService {
 
   private eventsSource: Subject<any> = new Subject<any>();
 
-  constructor() {
+  constructor(private heroService: HeroService) {
     this.events$ = this.eventsSource.asObservable();
   }
 
   createRandomEvent(cell: Cell) {
     this.eventsSource.next('Event');
+    // определение наличия события
+    let dice = Random.throwDiceD6();
+    if (dice >= 1) {
+      // TODO: >= 5
+      this.eventsSource.next({ type: TypeEvent.Smth, text: 'Произошло событие!' });
+      // определение масштабности события
+      dice = Random.throwDiceD6();
+      if (dice >= 50) {
+        // TODO: >= 5
+        // событие действует на всех персонажей сразу
+        this.eventsSource.next({ type: TypeEvent.AllHeroes, text: 'Событие действует на всех!' });
+        // TravelEvent.createEvent(player.heroes);
+      } else {
+        // шанс срабатывания события(для каждого игрока отдельно)
+        const chance = Random.throwDiceD6();
+        this.eventsSource.next({
+          type: TypeEvent.SeparateHeroes,
+          text: `Событие действует на каждого игрока отдельно! (сложность ${chance})`,
+        });
+        // перебор персонажей
+        const heroes: Hero[] = [];
+        this.heroService.heroes.forEach(hero => {
+          dice = Random.throwDiceD6();
+          this.eventsSource.next({
+            type: TypeEvent.CheckHero,
+            text: hero.name,
+          });
+          if (dice >= chance) {
+            heroes.push(hero);
+            this.eventsSource.next({
+              type: TypeEvent.SelectedHero,
+              text: hero.name + ' избран судьбой',
+            });
+          } else {
+            this.eventsSource.next({
+              type: TypeEvent.NotSelectedHero,
+              text: hero.name + ' стоит в стороне',
+            });
+          }
+        });
+        // TravelEvent.createEvent(heroes);
+      }
+    } else {
+      this.eventsSource.next({ type: TypeEvent.Nothing, text: 'Ничего не случилось' });
+    }
   }
-
 }
--- src/app/shared/services/random.ts
+++ src/app/shared/services/random.ts
@@ -2,7 +2,12 @@ export class Random {
   static getFloat(min, max): number {
     return Math.random() * (max - min) + min;
   }
+
   static getInt(min, max): number {
     return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  static throwDiceD6(): number {
+    return Random.getInt(1, 6);
   }
 }
