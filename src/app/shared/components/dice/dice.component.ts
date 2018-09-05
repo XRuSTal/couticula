fbe17d2267fdd433caf0b8b7f6a289553387d1cf
BarinDwalin <sergeishipwright@mail.ru>
2018-09-05 23:59:14 +0300

    Компонент кубика с анимацией броска

---
+++ src/app/shared/components/dice/dice.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { interval } from 'rxjs/observable/interval';
+import { finalize, take } from 'rxjs/operators';
+
+import { Random } from '@services';
+
+@Component({
+  selector: 'dice',
+  templateUrl: 'dice.component.html',
+})
+export class DiceComponent implements OnInit {
+  @Input()
+  value: number;
+  @Input()
+  backgroundColor = 'white';
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  animate(value: number, delay: number, animateInterval = 100) {
+    console.log('RESULT', value);
+    interval(animateInterval)
+      .pipe(
+        finalize(() => {
+          this.value = value;
+        }),
+        take(delay / animateInterval)
+      )
+      .subscribe(() => {
+        this.value = Random.throwDiceD6();
+        console.log(this.value);
+      });
+  }
+}
--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,9 +1,11 @@
 import { GoldComponent } from './gold/gold.component';
+import { DiceComponent } from './dice/dice.component';
 import { HeroInfoShortComponent } from './hero-info-short/hero-info-short.component';
 import { HeroesInfoShortComponent } from './heroes-info-short/heroes-info-short.component';
 import { ItemInfoShortComponent } from './item-info-short/item-info-short.component';
 
 const SHARED_COMPONENTS: any[] = [
+  DiceComponent,
   GoldComponent,
   HeroInfoShortComponent,
   HeroesInfoShortComponent,
@@ -12,6 +14,7 @@ const SHARED_COMPONENTS: any[] = [
 
 export {
   SHARED_COMPONENTS,
+  DiceComponent,
   GoldComponent,
   HeroInfoShortComponent,
   HeroesInfoShortComponent,
