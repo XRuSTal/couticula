05b0460b654f5be701fe0b0f50d5483f88380f98
BarinDwalin <sergeishipwright@mail.ru>
2018-09-25 22:17:06 +0300

    Удален неиспользуемый код

--- src/app/shared/models/item.ts
+++ src/app/shared/models/item.ts
@@ -27,11 +27,15 @@ export class Item {
 
   static getItemColor(value: number) {
     switch (value) {
-      case 6: return 'orangered';
-      case 5: return 'orange';
+      case 6:
+        return 'orangered';
+      case 5:
+        return 'orange';
       case 4:
-      case 3: return 'lightskyblue';
-      default: return 'white';
+      case 3:
+        return 'lightskyblue';
+      default:
+        return 'white';
     }
   }
 
@@ -61,66 +65,4 @@ export class Item {
   copy() {
     return new Item(this.type, this.value, this.name, this.img, this.description);
   }
-
-  /*public static createRandomEquipment(): Item {
-        var type: ItemType;
-        var name: string;
-        var value: number = Helpers.throwDice(6);
-        var image: ImageType;
-        var randomNum = Helpers.throwDice(6);
-        switch (randomNum) {
-            case 1: type = ItemType.Head; name = "Шлем"; image = ImageType.DiceHead;// TODO: вернуть иконки image = ImageType.BlackIcon;
-                break;
-            case 2: type = ItemType.Hands; name = "Наручи"; image = ImageType.DiceHands;//  image = ImageType.BlackIcon;
-                break;
-            case 3: type = ItemType.Legs; name = "Поножи"; image = ImageType.DiceLegs;//  image = ImageType.BlackIcon;
-                break;
-            case 4: type = ItemType.Body; name = "Доспех"; image = ImageType.DiceBody;//  image = ImageType.BlackIcon;
-                break;
-            case 5: type = ItemType.Weapon; name = "Оружие"; image = ImageType.DubinaOldJew;
-                break;
-            case 6:
-                var shieldType = Helpers.throwDice(3);
-                return new Shield(value, 2 * shieldType, "Щит", (shieldType === 1 ? ImageType.ShieldWeak : (shieldType === 1 ? ImageType.ShieldMedium : ImageType.ShieldStrong)));
-        }
-        return new Item(name, type, value, image);
-    }
-
-    public static createRandomBottle(): Item {
-        var type: ItemType;
-        var name: string;
-        var description: string;
-        var image: ImageType;
-        var randomNum = Helpers.throwDice(6);
-        switch (randomNum) {
-            case 1:
-            case 2:
-            case 3: type = ItemType.BottleOfHeal; name = listAbilities[AbilityType.HeroUseBottleOfHeal].name; description = listAbilities[AbilityType.HeroUseBottleOfHeal].description; image = ImageType.BottleOfHeal;
-                break;
-            case 4:
-            case 5: type = ItemType.BottleOfStan; name = listAbilities[AbilityType.HeroUseBottleOfStan].name; description = listAbilities[AbilityType.HeroUseBottleOfStan].description; image = ImageType.BottleOfStan;
-                break;
-            case 6: type = ItemType.BottleOfPoison; name = listAbilities[AbilityType.HeroUseBottleOfPoison].name; description = listAbilities[AbilityType.HeroUseBottleOfPoison].description; image = ImageType.BottleOfPoison;
-                break;
-        }
-        return new Item(name, type, null, image, description);
-    }
-    public static createRandomGoldBag(): Item {
-        var gold: number;
-        var name: string;
-        var description: string;
-        var sizeBag = Helpers.throwDice(6);
-        switch (sizeBag) {
-            case 1:
-            case 2:
-            case 3: gold = 100 * Helpers.throwDice(6); name = "Гроши";
-                break;
-            case 4:
-            case 5: gold = 200 * Helpers.throwDice(6); name = "Мошна";
-                break;
-            case 6: gold = 300 * Helpers.throwDice(6); name = "Кошель";
-                break;
-        }
-        return new Item(name, ItemType.Gold, gold, ImageType.Gold, description);
-    }*/
 }
