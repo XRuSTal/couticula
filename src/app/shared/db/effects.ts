c8550dd6f6490dad666c083c53ef0fa00ddd52de
BarinDwalin <sergeishipwright@mail.ru>
2018-10-16 08:56:24 +0300

    Добавлено описание эффектов

--- src/app/shared/db/effects.ts
+++ src/app/shared/db/effects.ts
@@ -15,16 +15,46 @@ export const Effects: Effect[] = [
     description: 'Шанс 1 из 6, урон 1',
   },
   {
+    effectType: EffectType.BlockHeal,
+    name: 'Проказа',
+    img: 'assets/img/effects/block-heal.jpg',
+    description: 'Не подвержен лечению до конца хода',
+  },
+  {
     effectType: EffectType.BreakingChests,
     name: 'Умелый взломщик',
     img: 'assets/img/effects/todo.jpg',
     description: '+1 к вскрытию сундуков',
   },
   {
+    effectType: EffectType.Course,
+    name: 'Проклятие',
+    img: 'assets/img/effects/course.jpg',
+    description: 'При атаке наносится минимальный урон (бросок равен 1).',
+  },
+  {
     effectType: EffectType.ForceBreakingChests,
     name: 'Крепкий взломщик',
     img: 'assets/img/effects/todo.jpg',
     description: '+1 к силовому вскрытию сундуков',
+  },
+  {
+    effectType: EffectType.HideCreature,
+    name: 'Существо скрыто',
+    img: 'assets/img/effects/todo.jpg',
+    description: 'Не может взаимодействовать с противником и стать целью атаки',
+  },
+  {
+    effectType: EffectType.Imbecility,
+    name: 'Скудоумие',
+    img: 'assets/img/effects/imbecility.jpg',
+    description: 'Атакует случайное существо',
+  },
+  {
+    effectType: EffectType.MagicProtection,
+    name: 'Магический щит',
+    img: 'assets/img/effects/magic-protection.jpg',
+    description: 'Защита от магических атак',
   },
   {
     effectType: EffectType.Poison1,
@@ -43,5 +73,17 @@ export const Effects: Effect[] = [
     name: 'Возрождение змия',
     img: 'assets/img/effects/todo.jpg',
     description: '',
+  },
+  {
+    effectType: EffectType.Slackness,
+    name: 'Вялость',
+    img: 'assets/img/effects/slackness.jpg',
+    description: 'Урон снижается на 2',
+  },
+  {
+    effectType: EffectType.Suppression,
+    name: 'Подавление',
+    img: 'assets/img/effects/suppression.jpg',
+    description: 'Получение двойного урона',
   },
 ];
