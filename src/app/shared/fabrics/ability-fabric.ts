ef602496734df516bd0027d3f0d06414df79875d
BarinDwalin <sergeishipwright@mail.ru>
2018-11-07 23:40:53 +0300

    Прототип базовой способности лечения

--- src/app/shared/fabrics/ability-fabric.ts
+++ src/app/shared/fabrics/ability-fabric.ts
@@ -8,7 +8,8 @@ import {
   ShopAbilitiesSpecial,
 } from '@shared/db';
 import { Ability, AbilityResult, AbilitySettings, Creature } from '@models';
-import { AbilityType } from '@enums';
+import { AbilityType, EffectType } from '@enums';
+import { Random } from '@services';
 
 export class AbilityFabric {
   private static abilities = new Map<
@@ -69,7 +70,7 @@ function heroBasicAttack(currentCreature: Creature, targetCreature: Creature) {
 }
 
 function heroBasicHeal(currentCreature: Creature, targetCreature: Creature) {
-  return basicHeal(currentCreature, targetCreature, {});
+  return basicHeal(currentCreature, targetCreature, { useWeapon: true, fixedHeal: null, diceHeal: null });
 }
 
 function monsterBasicAttack(currentCreature: Creature, targetCreature: Creature) {
@@ -80,6 +81,59 @@ function basicAttack(currentCreature: Creature, targetCreature: Creature, option
   return null;
 }
 
-function basicHeal(currentCreature: Creature, targetCreature: Creature, options): AbilityResult {
+function basicHeal(currentCreature: Creature, targetCreature: Creature, options: {
+  useWeapon: boolean;
+  fixedHeal: number;
+  diceHeal: number;
+}): AbilityResult {
+  const targetCreatureBefore = targetCreature/*.copy()*/;
+  const targetCreatureAfter = targetCreature;
+
+  const diceHeal = options.diceHeal || throwDiceDamage(currentCreature);
+  const weaponHeal = options.useWeapon && currentCreature.equipment.Weapon
+    ? currentCreature.equipment.Weapon.value
+    : 0;
+  const healCoefficient = getHealCoefficient(currentCreature, diceHeal);
+  const healValue = options.fixedHeal ? options.fixedHeal : (weaponHeal + diceHeal) * healCoefficient;
+
+  // const damage = - currentCreature.increaseHitpoint(healValue);
+
+  // Состояние броска
+  currentCreature.lastDiceDamage = null;
+  currentCreature.lastDiceTarget = null;
+  if (!options.fixedHeal) {
+    // abilityResult.diceDamage = diceHeal;
+    currentCreature.lastDiceDamage = diceHeal;
+  }
+
+  // Совместное лечение
+  if (currentCreature !== targetCreature) {
+    currentCreature.currentEffects.some(p => {
+      if (p.effectType === EffectType.HealWithAllies) {
+        // p.action(currentCreature);
+        return true;
+      }
+    });
+  }
+  // TODO: Возвращать Array<AbilityResult>, для информации по нескольким существам
   return null;
+}
+
+function throwDiceDamage(creature: Creature): number {
+  if (creature.isExistsEffect(EffectType.Course)) {
+    return 1;
+  }
+  const dice: number = Random.throwDiceD6();
+  return dice;
+}
+
+function getHealCoefficient(creature: Creature, dice: number) {
+  if (
+    creature.isExistsEffect(EffectType.WounderHeal) &&
+    (dice === 5 || dice === 6)
+  ) {
+    return 2;
+  } else {
+    return 1;
+  }
 }
