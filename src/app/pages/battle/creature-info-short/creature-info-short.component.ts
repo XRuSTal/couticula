e2e3bf31f176163d1b5344a0409b02e3a97173ac
BarinDwalin <sergeishipwright@mail.ru>
2019-02-23 01:45:59 +0300

    Стили маркера ранений

--- src/app/pages/battle/creature-info-short/creature-info-short.component.ts
+++ src/app/pages/battle/creature-info-short/creature-info-short.component.ts
@@ -8,6 +8,18 @@ import { CreatureView } from '@models';
 })
 export class CreatureInfoShortComponent {
   @Input() creature: CreatureView;
+  hitPointDiff: number;
 
+  get isPositive() {
+    return (this.hitPointDiff > 0);
+  }
+
+  get changeHitPoint() {
+    if (this.hitPointDiff > 0) {
+      return '+' + this.hitPointDiff;
+    } else {
+      return this.hitPointDiff;
+    }
+  }
   constructor() {}
 }
