82b56c39d0f13f5fe64ee435d30d7bd06b0cac86
BarinDwalin <sergeishipwright@mail.ru>
2019-08-09 01:07:05 +0300

    Исправлена страница настроек

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,26 +1,23 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 import { SettingsService } from '@services';
 import { GameMode } from '@shared/enums';
 
 @Component({
   selector: 'page-settings',
   templateUrl: 'settings.page.html',
+  styleUrls: ['settings.page.scss'],
 })
 export class SettingsPage {
   gameMode = GameMode;
 
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public settingsService: SettingsService
-  ) {}
+  constructor(public navCtrl: NavController, public settingsService: SettingsService) {}
 
   convertToNumber(event): number {
     return +event;
   }
 
   close() {
-    this.navCtrl.pop();
+    this.navCtrl.back();
   }
 }
